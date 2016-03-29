5c452a7c02bbc0ff19eff3241007220e54f36a47
wrtirado <wrtirado@gmail.com>
2016-03-29 14:39:03 -0600

    updated noteson the server.js file

--- server.js
+++ server.js
@@ -4,19 +4,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 
-//1 - We're going to redo PART II using fs.readFile and callbacks. This blocking code: var fileContents = fs.readFileSync('data.txt'); changes to this asynchronous code: fs.readFile('data.txt', function(err, data){ // do something with data here });
-
-//2 - Move res.header(...) and res.send(...) into the fs.readFile callback so that they are executed after the file is read.
-
-//3 - Restart your node server and request the localhost url again. You should see the same result as in PART II!
-
 
 // Create Express App Object \\
 var app = express();
 
 // read data.txt
 // Application Configuration \\
-// appluse gets called on EVERY request \\
+// app.use gets called on EVERY request \\
 
 app.use(logger('dev')); //<--
 app.use(bodyParser.json()); // <-- Looks for stringified JSON data and automatically parses it into an object
@@ -24,6 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true })); // <--  Body parser is a mod
 app.use(express.static(__dirname + '/public')); // <-- Used to serve CLIENT SIDE files (html, css, js, img)
 // Routes \\
 // Index/Home route => '/' \\
+
+// app.get that runs asyncronously from the rest of file, so that the rest of the file doesn't have to wait on it to finish running.
 app.get('/', function(req, res){
   fs.readFile('./data.txt', function(err, data){
     res.header('Content-Type', 'text/html')
